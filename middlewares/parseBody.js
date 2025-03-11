f2c1734d6201d0b8e0cfc86c29a5f9fb9927924e
David Valdez Gramajo <86501090+Davidvg29@users.noreply.github.com>
2025-03-11 18:38:47 -0300

    going up parseBody.js update

--- middlewares/parseBody.js
+++ middlewares/parseBody.js
@@ -6,7 +6,18 @@ module.exports = function parseBody(req){
         })
         req.on("end", ()=>{
             try {
-                resolve(JSON.parse(body))
+                const contentType = req.headers["content-type"]
+                if(contentType.includes("application/json")){
+                    resolve(JSON.parse(body))
+                }
+                else if(contentType.includes("application/x-www-form-urlencoded")){
+                    const parsedBody = new URLSearchParams(body);
+                    const result = {};
+                    for (const [key, value] of parsedBody.entries()) {
+                        result[key] = value;
+                    }
+                    resolve(result)
+                }
             } catch (error) {
                 reject(new Error("Error al parsear el body"))
             }
